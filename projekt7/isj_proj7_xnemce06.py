615bb5a3dc85d48db8b900b4c42eb0208e6b5920
Barbora Nemčeková <barbora.nemcekova@avast.com>
2021-02-28 10:32:25 +0100

    directory sorted

--- projekt7/isj_proj7_xnemce06.py
+++ projekt7/isj_proj7_xnemce06.py
@@ -16,15 +16,37 @@ def log_and_count(key=None, counts=my_counter):
 
 @log_and_count(key='basic functions', counts=my_counter)
 def f1(a, b=2):
+    """ test na f1
+    >>>f1(2)
+    'called f1 with (2,) and {}'
+    >>> f1(a=2, b=4)
+    'called f1 with () and {'a': 2, 'b': 4}'
+    """
     return a ** b
 
 @log_and_count(key = 'basic functions', counts = my_counter)
 def f2(a, b=3):
+    """ test na f1
+    >>>f2(2, b=4)
+    'called f2 with (2,) and {'b': 4}'
+    >>> f2(4)
+    'called f2 with (4,) and {}'
+    >>> f2(5)
+    'called f2 with (5,) and {}'
+    """
     return a ** 2 + b
 
 @log_and_count(counts = my_counter)
 def f3(a, b=5):
+    """ test na f1
+    >>>f3(5)
+    'called f3 with (5,) and {}'
+    >>> f3(5,4)
+    'called f3 with (5, 4) and {}'
+    
+    """
     return a ** 3 - b
+    
 f1(2)
 f2(2, b=4)
 f1(a=2, b=4)
